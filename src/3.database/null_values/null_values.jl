2a6ad2c4f95e922c5482aa1125eebd02cd40e433
Pere Giménez <gimenezfebrer@gmail.com>
2023-04-03 15:45:18 +0200

    fix null values

--- src/3.database/null_values/null_values.jl
+++ src/3.database/null_values/null_values.jl
@@ -1,14 +1,20 @@
+#=
+Inserting objects that have fields with `nothing` value results in a DB record with `NULL` value.
+=#
 using SearchLight
 using SearchLightSQLite
 include("app/resources/houses/Houses.jl")
 using .Houses
+# Find will convert the record field to the type of the default value in the struct.
+# We need to extend `convert` so that non-null fields are not converted to nothing.
+Base.convert(t::Type{Nothing}, x::Int64) = x
 
 SearchLight.Configuration.load() |> SearchLight.connect
 
-h = House(street="Barcelona", size=400, rooms=missing)
+h = House(street="Barcelona", size=400, rooms=nothing)
 save(h)
-h = House(street="Barcelona 2", size=400, rooms=nothing)
+h = House(street="Barcelona 2", size=400, rooms=4)
 save(h)
-h = House(street="Barcelona 3", size=400, rooms=4)
-save(h)
-# find(House, SQLWhereExpression("size == ?", 400))
+
+@show find(House, SQLWhereExpression("street ==? AND size == ?", "Barcelona", 400))
+@show find(House, SQLWhereExpression("street ==? AND size == ?", "Barcelona 2", 400))
