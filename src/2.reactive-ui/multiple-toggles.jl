f6635eee31f0cf057430bbd467dcf62b5f4d2455
Pere Giménez <gimenezfebrer@gmail.com>
2023-04-10 22:17:38 +0200

    multiple toggles

---
+++ src/2.reactive-ui/multiple-toggles.jl
@@ -0,0 +1,30 @@
+#=
+## Multiple toggles
+**Adding multiple toggles, each with its own state variable**
+=#
+
+module App
+using GenieFramework
+@genietools
+
+@app begin
+    @out toggle_list = ["red", "green", "yellow"]
+    @out toggle_states = []
+    @in toggle_name = "toggle"
+    @in process = false
+    @onchange process begin
+        toggle_list = vcat(toggle_list, [toggle_name])
+    end
+end
+
+@page("/", "multiple_toggles.html")
+Server.up()
+end
+
+#=
+**HTML code**
+
+```html
+:::multiple-toggles.html:::
+```
+=#
