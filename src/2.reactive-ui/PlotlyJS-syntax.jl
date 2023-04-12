a5388d0a5778b6a8b06685aa6d1ef1a81771be7f
Pere Giménez <gimenezfebrer@gmail.com>
2023-04-12 17:24:12 +0200

    plotlyjs syntax

---
+++ src/2.reactive-ui/PlotlyJS-syntax.jl
@@ -0,0 +1,22 @@
+#=
+## PlotlyJS syntax
+
+**Build plots with the PlotlyJS.jl syntax.**
+=#
+module App
+using GenieFramework, PlotlyBase
+@genietools
+
+trace1 = scatter(; x=1:4, y=[0, 2, 3, 5], fill="tozeroy")
+trace2 = scatter(; x=1:4, y=[3, 5, 1, 7], fill="tonexty")
+
+@handlers begin
+    @out plot = Plot([trace1, trace2], PlotlyBase.Layout(title="Filled line chart"))
+end
+
+ui() = plotly(:plot)
+
+@page("/", ui)
+Server.isrunning() || Server.up()
+end
+
