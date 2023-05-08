2eb81343ea65bde6df14975b7c13942c200053b4
Pere Giménez <gimenezfebrer@gmail.com>
2023-05-08 16:39:06 +0200

    frontmatter, html append

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## Genie Framework example library
 
-This repo contains short examples for Genie apps that showcase a single feature or use case.
-
+This repo contains short examples for Genie apps that showcase a single feature or use case. To run them, install the `GenieFramework` package, open a Julia REPL in the code directory, and execute with `include(example-name.jl)`.
 
 These examples are shown in the [Genie Cloud Resource Center](https://learn.geniecloud.io). The `build_docs.jl` script generates the .md files for the site using `Literate.jl`.
--- build_docs.jl
+++ build_docs.jl
@@ -5,9 +5,58 @@ sections = ["2.reactive-ui"]
 md_directory = "markdown/"
 docs_site_path = "../../geniedocssite/content/4.examples/"
 
+
+function add_frontmatter(content)
+    title_pattern = r"## (.*)"
+    titles = eachmatch(title_pattern, content)
+    title = first(titles).captures[1]
+    desc_pattern = r"\*\*(.*)\*\*"
+    desc_list = eachmatch(desc_pattern, content)
+    description = first(desc_list).captures[1]
+    content = """
+    ---
+    title: $title
+    description: $description
+    meta:
+        - name: 'og:title'
+        - content: '$title - Genie Cloud Resource Hub'
+    ---
+
+    $content
+    """
+    return content
+end
+
+# Add a link to the source code at the bottom of the page
 function add_source(content)
     content = content * "\n[source]($source)"
     return content
+end
+
+# Find all tags of the form :::tag_content::: in a string and extract their contents
+function find_tags(input_str::String)
+    tag_pattern = r":::([\w\s\._-]+(?:\.[\w]+)?):::"
+    tags = eachmatch(tag_pattern, input_str)
+    tag_contents = [tag.captures[1] for tag in tags]
+    return tag_contents
+end
+
+function add_file_contents(content)
+    root = joinpath(split(filepath, "/")[1:end-1]) * "/"
+    file_list = find_tags(content)
+    for file_name in file_list
+        @show file_name
+        file = open(root * file_name, "r")
+        code = read(file, String)
+        @show code
+        close(file)
+        content = replace(content, ":::$file_name:::" => code)
+    end
+    return content
+end
+
+function pipeline(content)
+    content |> add_file_contents |> add_source |> add_frontmatter
 end
 
 for section in sections
@@ -15,11 +64,11 @@ for section in sections
     for (root, dirs, files) in walkdir(directory)
         for file in files
             if endswith(file, ".jl")
-                filepath = joinpath(root, file)
+                global filepath = joinpath(root, file)
                 outputpath = joinpath(md_directory, root)
                 @show filepath
                 global source = "https://github.com/GenieFramework/CodeExamples/blob/main/src/$section/$file"
-                Literate.markdown(filepath, outputpath, flavor=Literate.CommonMarkFlavor(), credit=false, postprocess=add_source)
+                Literate.markdown(filepath, outputpath, flavor=Literate.CommonMarkFlavor(), credit=false, postprocess=pipeline)
             end
         end
     end
